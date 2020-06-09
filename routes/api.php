5840c7c7b9077e3dbd518e182c4c89fa253100f6
James Doe <james@gmail.com>
2020-06-09 12:23:37 +0400

    testing

--- routes/api.php
+++ routes/api.php
@@ -15,17 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth:api', 'verify_register'])->group(function () {
-    Route::get('user', 'Api\v1\UserController@show');
+//    Route::get('user', 'Api\v1\UserController@show');
 });
 
 
 //Authentication routes
 Route::group(['namespace' => 'Api\v1\Auth'], function () {
     Route::post('register', 'RegisterController@register');
-    Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
-    Route::get('email/verify/{id}/{hash}', 'VerificationController@verify')->name('verification.verify');
-    Route::post('login', 'LoginController@login');
-    Route::get('logout', 'LoginController@logout')->middleware('auth:api');
-    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
-    Route::post('password/reset', 'ResetPasswordController@reset');
+//    Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
+//    Route::get('email/verify/{id}/{hash}', 'VerificationController@verify')->name('verification.verify');
+//    Route::post('login', 'LoginController@login');
+//    Route::get('logout', 'LoginController@logout')->middleware('auth:api');
+//    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
+//    Route::post('password/reset', 'ResetPasswordController@reset');
 });
