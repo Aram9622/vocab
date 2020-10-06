05df921b11179a3888e3976df25a62a69cff7df2
Your Name <you@example.com>
2020-10-06 14:37:43 +0400

    up

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware(['auth:api'])->group(function () {
 
         Route::prefix('news')->group(function () {
             Route::get('/', 'NewsController@index')->name('news.index');
-            Route::get('view', 'NewsController@view')->name('news.view');
+            Route::get('view/{id}', 'NewsController@view')->name('news.view');
         });
     });
 
