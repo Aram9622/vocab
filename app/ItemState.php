d4ce34984a5dd137c8d584facaa58706dbea9779
Like To Dev <developer@gmail.com>
2020-10-23 16:01:07 +0400

    up

--- app/ItemState.php
+++ app/ItemState.php
@@ -15,6 +15,8 @@ class ItemState extends Model
     {
         $className = get_class($this->modelFactory($this->type));
 
+        echo $className; die;
+
         return $this->hasOne($className, 'id', 'item_id');
     }
 }
