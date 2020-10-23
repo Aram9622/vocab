64966eca2069584e4af577a87055b444cbef96bc
Like To Dev <developer@gmail.com>
2020-10-23 09:56:42 +0400

    up

--- database/migrations/2020_10_22_130725_image_to_flashcard_groups_table.php
+++ database/migrations/2020_10_22_130725_image_to_flashcard_groups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ImageToFlashcardsTable extends Migration
+class ImageToFlashcardGroupsTable extends Migration
 {
     /**
      * Run the migrations.
