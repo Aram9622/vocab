20982dc59bfcf560f0b64d723e2dc56b393d4f8c
Like To Dev <developer@gmail.com>
2020-10-27 12:29:37 +0400

    up

--- app/Http/Controllers/Api/v1/Traits/Categories.php
+++ app/Http/Controllers/Api/v1/Traits/Categories.php
@@ -109,7 +109,7 @@ trait Categories
                 $model->words_es = $model->conversation_es ?: $model->exercise_es ?: $model->phrase_es ?: $model->story_es ?: $model->verb_es ?: null;
             }
         } catch (\Exception $e) {
-
+            dd($e);
         }
     }
 
