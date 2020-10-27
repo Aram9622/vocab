f310f3996a1e0b5667ef68a8382beac60bf1f5c2
Like To Dev <developer@gmail.com>
2020-10-27 15:36:07 +0400

    up

--- app/Http/Controllers/Api/v1/Traits/Categories.php
+++ app/Http/Controllers/Api/v1/Traits/Categories.php
@@ -54,7 +54,9 @@ trait Categories
         $intermediate = $this->index('intermediate', $category, $sub_category);
         $advanced = $this->index('advanced', $category, $sub_category);
 
-        return compact('beginner', 'intermediate', 'advanced');
+        $all = array_merge($beginner->toArray(), $intermediate->toArray(), $advanced->toArray());
+
+        return compact('all', 'beginner', 'intermediate', 'advanced');
     }
 
     /**
