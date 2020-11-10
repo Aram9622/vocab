0b1458787acbaa22203b9ab193f0ae72fa10ad59
dev <miqoo1996@mail.ru>
2020-11-10 10:59:52 +0400

    up

--- app/Services/Card.php
+++ app/Services/Card.php
@@ -49,7 +49,7 @@ class Card
 
     public function all()
     {
-        return $this->cardItemModel->with('itemState')->has('itemState')->where('user_id', auth()->id())->get()->map(function ($model) {
+        return $this->cardItemModel->with(['itemState', 'itemState.joinedModel.category'])->has('itemState')->get()->map(function ($model) {
             $model = $model->itemState->joinedModel;
             $this->correctAttributes($model);
             return $model;
