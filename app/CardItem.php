e3de232fbc6faa77b3aea3b2817a4c04174fc323
dev <miqoo1996@mail.ru>
2020-11-10 11:14:35 +0400

    up

--- app/CardItem.php
+++ app/CardItem.php
@@ -13,6 +13,6 @@ class CardItem extends Model
     {
         return $this->hasOne(ItemState::class, 'id', 'item_state_id')
             ->selectRaw('item_states.type as tbl')
-            ->leftJoin(DB::raw('words'), 'item_states.item_id', 'tbl.id');
+            ->leftJoin(DB::raw('words'), 'item_states.item_id', 'words.id');
     }
 }
