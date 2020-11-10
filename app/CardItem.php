1cf2da9e1fa3e751a3b71786d1003b89270a9600
dev <miqoo1996@mail.ru>
2020-11-10 11:07:11 +0400

    up

--- app/CardItem.php
+++ app/CardItem.php
@@ -11,6 +11,6 @@ class CardItem extends Model
 
     public function itemState()
     {
-        return $this->hasOne(ItemState::class, 'id', 'item_state_id')->leftJoin(DB::raw('item_states.type as tbl'), 'item_states.item_id', 'tbl.id');
+        return $this->hasOne(ItemState::class, 'id', 'item_state_id')->leftJoin(DB::raw('SELECT (item_states.type) as tbl'), 'item_states.item_id', 'tbl.id');
     }
 }
