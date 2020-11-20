4a22b2e8c3fba4aa795e63b9cab2435cde1a411a
dev <miqoo1996@mail.ru>
2020-11-20 14:45:44 +0400

    up

--- app/Http/Controllers/Api/v1/SiteController.php
+++ app/Http/Controllers/Api/v1/SiteController.php
@@ -139,7 +139,7 @@ class SiteController extends ApiController
 
         $learnedCount = $query->count();
 
-        return ['allTimeCount' => $count, 'learnedCount' => $learnedCount, 'learned' => array_values($learned)];
+        return ['learnedCount' => $learnedCount, 'learned' => array_values($learned)];
     }
 
     public function statistics()
