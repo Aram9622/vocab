4149e56a8c3b2c3db1f3b2bdd0681cbcecf7da13
dev <miqoo1996@mail.ru>
2020-11-23 09:41:11 +0400

    up

--- app/Http/Controllers/Api/v1/SiteController.php
+++ app/Http/Controllers/Api/v1/SiteController.php
@@ -115,7 +115,7 @@ class SiteController extends ApiController
                 $date = Carbon::now()->startOfYear()->toDateString();
             }
 
-            $query = ItemState::query()->where('user_id', 4)
+            $query = ItemState::query()->where('user_id', 7)
                 ->selectRaw('type, DATE(updated_at) as date')
                 ->where('current_state', 'learned')
                 ->whereDate('updated_at', '>=', $date)
