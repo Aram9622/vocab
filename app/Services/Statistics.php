d6e2a2eb4e99c715dc4752cfadb02104a554ce1c
dev <miqoo1996@mail.ru>
2020-11-23 13:01:37 +0400

    up

--- app/Services/Statistics.php
+++ app/Services/Statistics.php
@@ -13,7 +13,7 @@ class Statistics
 
     public function getStatisticsByInterval($interval, $_date = null)
     {
-        $_date = $_date ?: Carbon::now();
+        $_date = $_date ?: (new Carbon())->now();
 
         $startOMonth = clone $_date->startOfMonth();
         $endOMonth = clone $_date->endOfMonth()->addDays(1);
