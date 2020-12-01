0a57f14956a28e7166c213d75f93d4a295a39475
dev <miqoo1996@mail.ru>
2020-12-01 15:39:19 +0400

    up

--- app/Http/Requests/StoreConversationRequest.php
+++ app/Http/Requests/StoreConversationRequest.php
@@ -26,8 +26,8 @@ class StoreConversationRequest extends FormRequest
         return [
             'conversation_en'   => 'required|string|max:255',
             'conversation_es'   => 'required|string|max:255',
-//            'record_en' =>'nullable|file|mimes:audio/mpeg,mpga,mp3,wav,aac',
-//            'record_es' =>'nullable|file|mimes:audio/mpeg,mpga,mp3,wav,aac',
+            'record_en' =>'required|file|mimes:audio/mpeg,mpga,mp3,wav,aac',
+            'record_es' =>'required|file|mimes:audio/mpeg,mpga,mp3,wav,aac',
             'eg_link'   => 'required|string|max:255',
             'image'     => 'required|file|mimes:jpeg,jpg,png,gif',
         ];
